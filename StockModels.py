c2bcb83c5a26d1ce205c33fe0d27326048234665
Eakkarat-Patt <73377720+Eakkarat-Patt@users.noreply.github.com>
2021-10-18 16:40:59 +0700

    -error fixed

--- StockModels.py
+++ StockModels.py
@@ -102,6 +102,9 @@ class GeneralizedBrownianMotion(StockPricesModel):
     def GetZ(self):
         return self.Z
 
+    def GetPq(self):
+        return self.Pq
+
     def GetOmg(self):
         return self.Omg
 
@@ -109,6 +112,7 @@ class GeneralizedBrownianMotion(StockPricesModel):
         self.c = (np.pi * gamma(1 / (q - 1) - 0.5) ** 2) / ((q - 1) * gamma(1 / (q - 1)) ** 2)
         self.B = self.c ** ((1 - q) / (3 - q)) * ((2 - q) * (3 - q) * self.GetTime()) ** (-2 / (3 - q))
         self.Z = ((2 - q) * (3 - q) * self.c * self.GetTime()) ** (1 / (3 - q))
+        self.Pq[:, 0] = ((1 - self.GetB()[0] * (1 - q) * self.GetOmg()[:, 0] ** 2) ** (1 / (1 - q))) / self.GetZ()[0]
         self.S[:, 0] = S0
 
         for i in range(1, self.GetNumSteps()):
@@ -118,59 +122,47 @@ class GeneralizedBrownianMotion(StockPricesModel):
 
             self.Pq[:, i] = ((1 - self.GetB()[i] * (1 - q) * self.GetOmg()[:, i] ** 2) ** (1 / (1 - q))) / self.GetZ()[i]
 
-            self.S[:, i] = self.S[:, i - 1] + self.S[:, i - 1] * (self.GetTime()[i]-self.GetTime()[i-1])\
-                           * (r + 0.5 * sigma ** 2 * self.Pq[:, i - 1] ** (1 - q)) + sigma * self.S[:, i - 1]\
-                           * (self.GetOmg()[:, i] - self.GetOmg()[:, i - 1])
+            self.S[:, i] = self.S[:, i - 1] + r * self.S[:, i - 1] * (self.GetTime()[i]-self.GetTime()[i-1])\
+                           + sigma * self.S[:, i - 1] * (self.GetOmg()[:, i] - self.GetOmg()[:, i - 1])
 
 
-numPaths = 10
-dt = 0.001
-t0 = 1e-20
-T = 1
-numSteps = int(T / dt)
-r = 0.05
-sigma = 0.2
-S0 = 50
-q = 1.6
+# numPaths = 100000
+# dt = 0.001
+# t0 = 1e-20
+# T = 1
+# numSteps = int(T / dt)
+# r = 0.05
+# sigma = 0.2
+# S0 = 50
+# q = 1.3
 #
-# p1 = GeometricBrownianMotion()
-# p1.generateWiener(numPaths, numSteps, t0, T)
+#
+# w1 = WienerProcess()
+# w1.generateWiener(numPaths, numSteps, t0, T)
+#
+# p1 = GeometricBrownianMotion(w1)
 # p1.generateStockPath(r, sigma, S0)
 #
-# p2 = NonGaussianBrownianMotion()
-# p2.generateWiener(numPaths, numSteps, t0, T)
-# p2.generateOmega(q)
+# p2 = GeneralizedBrownianMotion(w1)
 # p2.generateStockPath(r, sigma, S0, q)
 
-w1 = WienerProcess()
-w1.generateWiener(numPaths, numSteps, t0, T)
-
-p1 = GeometricBrownianMotion(w1)
-p1.generateStockPath(r, sigma, S0)
-
-p2 = GeneralizedBrownianMotion(w1)
-p2.generateStockPath(r, sigma, S0, 1.1)
-
-def logReturn(func):
-    df = pd.DataFrame({'time': func.getTime(),
-                       'stock price': func.getS()[0, :]})
+def logReturn(func1, func2):
+    df = pd.DataFrame({'time': func1.GetTime(),
+                       'stock price': func1.GetS()[MaxDifference(func1, func2), :]})
     df['daily log return'] = np.log(df['stock price'] / df['stock price'].shift(1))
     df['daily log return'] = (df['daily log return'] - df['daily log return'].mean()) / df['daily log return'].std()  # standardization
     return df['daily log return']
 
 
-def distPlot(func1, func2):
+def distPlot(func1, func2, logScale=False):
     plt.figure(figsize=(8, 5), dpi=500)
-    sns.histplot(func1, binwidth=0.2, color='r', binrange=[-5, 5], label='Tsallis$', stat='density', log_scale=(False, False))
-    sns.histplot(func2, binwidth=0.2, binrange=[-5, 5],label='Gaussian', stat='density', log_scale=(False, False))
+    sns.histplot(func1, binwidth=0.2, color='r', binrange=[-10, 10], label='Tsallis', stat='density', log_scale=(False, logScale))
+    sns.histplot(func2, binwidth=0.2, binrange=[-10, 10], label='Gaussian', stat='density', log_scale=(False, logScale))
     plt.xlim(-10, 10)
     plt.legend()
     plt.title('Tsallis Distribution')
     plt.show()
 
-
-#distPlot(logReturn(p2), logReturn(p1))
-#distPlot(p2.getOmg()[:,-1], p2.getW()[:,-1])
 
 def pathPlot(x, y1, numPaths=20):
     plt.figure(figsize=(8, 5), dpi=500)
@@ -187,7 +179,6 @@ def pathPlot2(x, y1, y2):
     plt.plot(x, y1[0, :], label='GBM')
     plt.plot(x, y2[0, :], label='Generalized GBM q = {}'.format(q))
     plt.xlim([0.0, x[-1]])
-    plt.ylim([46, 70])
     plt.title('Stock price path')
     plt.ylabel('Price')
     plt.xlabel('Time')
@@ -195,34 +186,10 @@ def pathPlot2(x, y1, y2):
     plt.show()
 
 
-# pathPlot(p1.getTime(), p1.getS(), numPaths=1)
-# pathPlot(p2.getTime(), p2.getS(), numPaths=1)
-# distPlot(logReturn(p2), logReturn(p1))
-# distPlot(p2.getOmg()[:,-1], p2.getW()[:,-1])
-
-
-
-# def varPlot(func, q):
-#     exact = TsallisVar(q, func.getTime())
-#     emp = varOmg(func)
-#     err = (exact - emp) * 100 / exact
-#     plt.figure(figsize=(8, 5), dpi=500)
-#     plt.plot(func.getTime(), err)
-#     plt.title('Variance percentage error')
-#     plt.xlabel('Time')
-#     plt.ylabel('Error')
-#     plt.show()
-#
-#
-# def TsallisVar(q, t):
-#     c = (np.pi * gamma(1 / (q - 1) - 0.5) ** 2) / ((q - 1) * gamma(1 / (q - 1)) ** 2)
-#     B = c ** ((1 - q) / (3 - q)) * ((2 - q) * (3 - q) * t) ** (-2 / (3 - q))
-#     return 1 / ((5 - 3 * q) * B)
-#
-#
-# def varOmg(func):
-#
-#     var_t = np.zeros([func.getnumSteps()])
-#     for i in range(1, func.getnumSteps()):
-#         var_t[i] = func.getOmg()[:, i].var()
-#     return var_t
+def MaxDifference(func1, func2):
+    diff = np.zeros([func1.GetS().shape[0]])
+    finalS1 = func1.GetS()[:, -1]
+    finalS2 = func2.GetS()[:, -1]
+    for i in range(func1.GetS().shape[0]):
+        diff[i] = abs(finalS1[i]-finalS2[i])
+    return np.argmax(diff)
